bb441beba7dc2d3f269a4a04a6671eec90fe4537
Luiz Porto <piilamster@gmail.com>
2023-01-28 19:41:18 -0300

    platform collision

--- index.js
+++ index.js
@@ -37,8 +37,27 @@ class Player{
     }
 }
 
+class Platform {
+    constructor() {
+       this.position = {
+        x: 200,
+        y: 100
+       } 
+
+       this.width = 200
+       this.height = 20
+    }
+
+    draw() {
+        c.fillStyle = 'blue'
+       c.fillRect(this.position.x, this.position.y, this.width, this.height) 
+    }
+}
+
 // Player moviment
 const player =  new Player()
+const platform = new Platform()
+
 const keys = {
     right: {
         pressed: false
@@ -52,12 +71,18 @@ function animate() {
     requestAnimationFrame(animate)
     c.clearRect(0, 0, canvas.width, canvas.height)
     player.update();
+    platform.draw()
 
     if (keys.right.pressed) {
         player.velocity.x = 5
     } else if (keys.left.pressed) {
         player.velocity.x = -5
     } else player.velocity.x = 0
+
+    // platform collision detection
+    if (player.position.y + player.height <= platform.position.y && player.position.y + player.height + player.velocity.y >= platform.position.y && player.position.x + player.width >= platform.position.x && player.position.x <= platform.position.x + platform.width) {
+        player.velocity.y = 0
+    }
 }
 
 animate()
