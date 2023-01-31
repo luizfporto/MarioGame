6dfd5e8a2d712eb17e21dd69abd0de9be418703e
Luiz Porto <piilamster@gmail.com>
2023-01-31 15:44:18 -0300

    fine-tuning

--- src/js/canvas.js
+++ src/js/canvas.js
@@ -164,7 +164,9 @@ function animate() {
     
     if (keys.right.pressed && player.position.x < 400) {
         player.velocity.x = player.speed
-    } else if (keys.left.pressed && player.position.x > 100) {
+    } else if ((keys.left.pressed && player.position.x > 100)  || 
+      (keys.left.pressed && scrollOffset === 0 && player.position.x > 0) 
+    ){
         player.velocity.x = -player.speed
     } else {
         player.velocity.x = 0
@@ -177,7 +179,7 @@ function animate() {
             genericObjects.forEach((genericObject) => {
                 genericObject.position.x -= player.speed * 0.66
             })
-        } else if (keys.left.pressed) {
+        } else if (keys.left.pressed && scrollOffset > 0) {
             scrollOffset -= player.speed
             platforms.forEach(platform => {
                 platform.position.x += player.speed
